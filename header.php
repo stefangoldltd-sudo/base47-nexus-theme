b1b0aca1ef47a6e47e5b993195f7f9e439a0731f
stefangoldltd-sudo <stefangoldltd@gmail.com>
2025-11-20 11:08:11 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,7 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit;
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo('charset'); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
