74519a58406cca05772802cadc494771de26f4f0
stefangoldltd-sudo <stefangoldltd@gmail.com>
2025-11-20 11:10:27 +0200

    Update header.php

--- header.php
+++ header.php
@@ -2,6 +2,12 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Prevent WordPress from adding weird formatting -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Minimal theme CSS reset (loaded in functions.php) -->
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
